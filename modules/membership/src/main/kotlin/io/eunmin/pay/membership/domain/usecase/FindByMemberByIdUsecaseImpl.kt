5cc84c8001beb80ceed484dca3d2bfe82c88f68d
eunmin <eunmin@blocksquare.tech>
2024-09-11 18:53:42 +0900

    add member service

--- modules/membership/src/main/kotlin/io/eunmin/pay/membership/domain/usecase/FindByMemberByIdUsecaseImpl.kt
+++ modules/membership/src/main/kotlin/io/eunmin/pay/membership/domain/usecase/FindByMemberByIdUsecaseImpl.kt
@@ -1,6 +1,8 @@
 package io.eunmin.pay.membership.domain.usecase
 
 import io.eunmin.pay.common.annotation.Usecase
+import io.eunmin.pay.common.error.NotFoundException
+import io.eunmin.pay.common.error.enums.ErrorCode
 import io.eunmin.pay.membership.domain.input.FindMemberByIdUsecase
 import io.eunmin.pay.membership.domain.model.Member
 import io.eunmin.pay.membership.domain.model.query.MemberQuery
@@ -10,7 +12,6 @@ import io.eunmin.pay.membership.domain.output.MemberPort
 internal class FindByMemberByIdUsecaseImpl(
     private val memberPort: MemberPort
 ): FindMemberByIdUsecase() {
-
     override suspend fun execute(params: MemberQuery.FindById): Member =
         memberPort.findMemberById(params)?: throw NotFoundException(ErrorCode.MEMBER_NOT_FOUND)
 }
